5d8809a0f8b442dfa5e90ebd846be5e415bd3eac
Paul <memo@sibnet.ru>
2014-02-24 14:48:35 +0600

    command line arguments support added

--- ListDownloader.CUI/Program.cs
+++ ListDownloader.CUI/Program.cs
@@ -15,23 +15,51 @@ namespace ListDownloader.CUI
     {
         static void Main(string[] args)
         {
-            var kernel = setupNinject(5);
+            string sourceFilePath, destDirectoryPath;
+            int threadCount;
+
+            if (args.Length == 3)
+            {
+                sourceFilePath = args[0];
+                destDirectoryPath = args[1];
+                threadCount = Int32.Parse(args[2]);
+            }
+            else
+            {
+                Console.WriteLine("Arguments must be: " + System.AppDomain.CurrentDomain.FriendlyName + " <0> <1> <2>");
+                Console.WriteLine("<0> Path to links file");
+                Console.WriteLine("<1> Destination dir (Created if not exists)");
+                Console.WriteLine("<2> Threads count");
+                return;
+            }
+
+            if (! ( checkSourceFile(sourceFilePath) && 
+                checkDestDirectory(destDirectoryPath) && 
+                checkTreadCount(threadCount)) )
+            {
+                return;
+            }
+            
+            Console.WriteLine("Starting downloading..");
+
+            var kernel = setupNinject(threadCount);
 
             var downloader = kernel.Get<IDownloader>();
-            var links = File.ReadAllLines(@"");
+            var links = File.ReadAllLines(sourceFilePath);
 
             var timer = new Stopwatch();
             downloader.FileDownloaded += (o, e) => { showProgress(e.FilesDownloaded, e.FilesTotal, timer); };
 
             timer.Start();
-            downloader.DownloadAll(links, @"");
+            downloader.DownloadAll(links, destDirectoryPath);
 
             Console.ReadKey();
+            timer.Stop();
         }
 
         private static void showProgress(int downloaded, int total, Stopwatch t)
         {
-            Console.Write("\r Loaded: [{0} / {1}], {2:#00.}% total.", downloaded, total, ((double)downloaded / total) * 100);
+            Console.Write("\r Loaded: [{0} / {1}], {2:##0.}% total.", downloaded, total, ((double)downloaded / total) * 100);
 
             if (downloaded == total)
             {
@@ -53,6 +81,50 @@ namespace ListDownloader.CUI
                 );
 
             return kernel;
+        }
+
+        private static bool checkSourceFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Source file doesnt exist...");
+                return false;
+            }
+                
+            return true;
+        }
+
+        private static bool checkDestDirectory(string destDirectoryPath)
+        {
+            if (!Directory.Exists(destDirectoryPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(destDirectoryPath);
+                    Console.WriteLine("Directory " + destDirectoryPath + " created!");
+                }
+                catch
+                {
+                    Console.WriteLine("Cant find or create directory. Aborting...");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool checkTreadCount(int threadCount)
+        {
+            if (threadCount <= 0)
+            {
+                Console.WriteLine("Threads number cant be 0 or less...");
+                return false;
+            }
+                
+
+            System.Net.ServicePointManager.DefaultConnectionLimit = threadCount;
+
+            return true;
         }
     }
 }
